06b9c26e4e937995272187c08c9a7528b4bb712d
Kyrch <kyrchk5@gmail.com>
2022-09-08 13:39:30 -0300

    nothing

--- src/events/modalIndica.js
+++ src/events/modalIndica.js
@@ -1,16 +1,17 @@
 module.exports = {
     async createEvent(modal) {
+        const { customId, fields, guild, user } = modal
         if (!modal.isModalSubmit()) return
-        if (modal.customId != 'modal-indica') return
+        if (customId != 'modal-indica') return
 
         try { await modal.deferReply({ ephemeral: true }) } catch (err) {}
 
-        let name = modal.fields.getTextInputValue('name-midia')
-        let sinopse = modal.fields.getTextInputValue('sinopse-midia')
-        let link = modal.fields.getTextInputValue('link-midia')
+        let name = fields.getTextInputValue('name-midia')
+        let sinopse = fields.getTextInputValue('sinopse-midia')
+        let link = fields.getTextInputValue('link-midia')
 
         await modal.followUp({ content: `**${name}** foi enviado à administração.`, ephemeral: true })
-        let channel = modal.guild.channels.cache.find(c => c.id == '852696340264910898')
-        channel.send({ content: `**Nome:** ${name}\n\n**Sinopse:** ${sinopse}\n\n**Link:** ${link}\n\n**Indicado por:** <@!${modal.user.id}>` })
+        let channel = guild.channels.cache.find(c => c.id == '852696340264910898')
+        channel.send({ content: `**Nome:** ${name}\n\n**Sinopse:** ${sinopse}\n\n**Link:** ${link}\n\n**Indicado por:** <@!${user.id}>` })
     }
 }
